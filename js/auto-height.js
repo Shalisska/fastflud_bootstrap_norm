ff464f3ee7335454399e974790c9edd4ea5da229
Alena <alena.simonova83@gmail.com>
2015-03-11 19:13:55 +0300

    comments

---
+++ js/auto-height.js
@@ -0,0 +1,15 @@
+function setEqualHeight(columns) {
+    var tallestcolumn = 0;
+    columns.each(function () {
+        currentHeight = $(this).height();
+        if (currentHeight > tallestcolumn) {
+            tallestcolumn  = currentHeight;
+        }
+    });
+        
+    columns.height(tallestcolumn);
+}
+
+$(document).ready(function () {
+    setEqualHeight($('main  > div').not($('.row')));
+});
