f52529f2535c38a186a499994feac04ac957c98b
Alena <alena.simonova83@gmail.com>
2015-03-12 12:42:51 +0300

    111

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//рейтинг коммента
+(function (document) {
+var plus = document.querySelectorAll('.comments-item__counter--plus');
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+})(document);
+
+
+
+
+
+
+//(function (document) {
+//    
+//})(document);
