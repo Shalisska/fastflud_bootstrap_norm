18799fe79dccb1b55f026c796320586ede6ad146
Alena <alena.simonova83@gmail.com>
2015-03-12 19:08:59 +0300

    1203

--- js/common.js
+++ js/common.js
@@ -85,5 +85,27 @@ $(document).ready(function() {
 //		});
 //		return false;
 //	});
+    
+    //кнопка Наверх
+    $(function(){
+  $.fn.scrollToTop=function(){
+    $(this).hide().removeAttr("href");
+    if($(window).scrollTop()!="0"){
+        $(this).fadeIn("slow")
+  }
+  var scrollDiv=$(this);
+  $(window).scroll(function(){
+    if($(window).scrollTop()=="0"){
+    $(scrollDiv).fadeOut("slow")
+    }else{
+    $(scrollDiv).fadeIn("slow")
+  }
+  });
+    $(this).click(function(){
+      $("html, body").animate({scrollTop:0},"slow")
+    })
+  }
+});
+$(function() {$("#toTop").scrollToTop();});
 
 });
--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
-//рейтинг коммента
+//дата в верхнем меню - Новенькое
 (function (document) {
-var plus = document.querySelectorAll('.comments-item__counter--plus');
-
-
-
-
-
-
-};
-
-
-
-
-
-
-
-
-
+function clock() {
+    var d = new Date();
+    var month_num = d.getMonth()
+    var day = d.getDate();
+    var month = new Array("января", "февраля", "марта", "апреля", "мая", "июня",
+"июля", "августа", "сентября", "октября", "ноября", "декабря");
+    if (day <= 9) day = "0" + day;
+    
+    var date = document.querySelector('.top-menu .date');
+    
+    var date_day = date.querySelector('.date__day');
+    var date_month = date.querySelector('.date__month');
+    
+    date_day.innerHTML = day;
+    date_month.innerHTML = month[month_num];
+}
+clock();
 })(document);
+
+
+
+
+
 
 
 
